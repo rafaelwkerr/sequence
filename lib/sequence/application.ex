f936d6362ff962b5c44941927c9aa2730ab11ae4
Rafael Kerr <rafael.kerr@gmail.com>
2019-11-26 11:57:30 -0300

    🎉 :octopus: Initial commit
    startup project

---
+++ lib/sequence/application.ex
@@ -0,0 +1,16 @@
+defmodule Sequence.Application do
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      { Sequence.Stash,  123},
+      { Sequence.Server, nil},
+    ]
+
+    opts = [strategy: :rest_for_one, name: Sequence.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+end
